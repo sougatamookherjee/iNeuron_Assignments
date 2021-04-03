03ff558a2d8a7779b242d76839d29da421c9375a
sougatamookherjee <72007526+sougatamookherjee@users.noreply.github.com>
2021-04-03 12:49:38 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+1. In the below elements which of them are values or an expression? eg:- values can be integer or string and expressions will be mathematical operators.
+* 
+'hello'
+-87.8
+- 
+/ 
++	
+6 Ans;
+* -->> expression
+
+'hello' -->> Values
+
+-87.8 -->> Values
+
+- -->> expression
+
+/ -->> expression
+
++ -->> expression
+
+6 -->> Values2. What is the difference between string and variable?Ans; Variable -->> it is a store of information 
+
+String -->> it is type of information 
+
+A string is usually a word enclosed in " ". ex x = "Data is gold"
+
+x is the variable and "Data is gold" is string.3. Describe three different data types.Ans; Integers -->> these are positive or negative whole numbers with no decimal points.
+
+String -->> they are sequence of charecters which are immutable.
+
+Boolean -->> represents the truth value of an expression. True or False4. What is an expression made up of? What do all expressions do?Ans; Expressions are bassically represantation of values. There are three kinds of expression 1) Arithmetic expression, 2)Charecter expression & 3) Logical expression.
+
+An expression is an instruction that combines values and operators and evaluates down to a single value.5. This assignment statements, like spam = 10. What is the difference between an expression and a statement?Ans; Statements represent an action or command whereas Expressions are combination of variables, opeartions and values.6. After running the following code, what does the variable bacon contain?
+bacon = 22
+bacon + 1Ans; 227. What should the values of the following two terms be?
+'spam' + 'spamspam'
+'spam' * 3Ans; 'spam' + 'spamspam' -->> 'spamspamspam'
+
+'spam' * 3 -->> 'spamspamspam'8. Why is eggs a valid variable name while 100 is invalid?Ans; Becasue variables cannot begin with a number.9. What three functions can be used to get the integer, floating-point number, or string version of a value?Ans; int(), float() and str()10. Why does this expression cause an error? How can you fix it?
+'I have eaten ' + 99 + ' burritos.'Ans; Because it can only concatenate str to str not int(99)
+
+It can be fixed if 99 is changed to str ('99').
+
+'I have eaten ' + '99' + ' burritos.'
